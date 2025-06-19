d32d30f3a3e0e5905f69674f9d23ab81d2d0c819
Kenny2006-PE <kenny.palacin@tecsup.edu.pe>
2025-06-18 20:21:33 -0500

    login base

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+// src/app/dashboard/page.tsx
+function DashboardPage(){
+    return (
+        <div>Dashboard Page</div>
+    )
+}
+
+export default DashboardPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+// src/app/layout.tsx
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import Navbar from "@/components/Navbar";
+import { Providers } from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +29,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <Providers>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
